e89f5c07ac4c37ca40f49597d17d6eead9c095d8
claudiawheeler <119689117+claudiawheeler@users.noreply.github.com>
2022-12-16 16:07:37 -0500

    Updated layout of list

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 let pokemonList: [
-    { name: 'Bulbasaur', type: 'grass', height: 2.2 }
-    { name: 'Mew', type: 'psychic', height: 0.4 }
-    { name: 'Zubat', type: 'poison' 'flying', height: 0.8 }
+    { 
+        name: 'Bulbasaur',
+        type: ['grass'],
+        height: 2.2
+    },
+    {
+        name: 'Mew',
+        type: ['psychic'],
+        height: 0.4
+    },
+    {
+        name: 'Zubat',
+        type: ['poison', 'flying'],
+        height: 0.8
+    }
 ]
