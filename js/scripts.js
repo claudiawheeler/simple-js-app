5355f3e0bb8e0b978d52a4a4fe854cf80ca9a595
claudiawheeler <119689117+claudiawheeler@users.noreply.github.com>
2022-12-16 16:01:00 -0500

    Added Pokemon List

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-
-let favoriteFood = 'Mashed Potatoes';
-document.write(favoriteFood);
+let pokemonList: [
+    { name: 'Bulbasaur', type: 'grass', height: 2.2 }
+    { name: 'Mew', type: 'psychic', height: 0.4 }
+    { name: 'Zubat', type: 'poison' 'flying', height: 0.8 }
+]
